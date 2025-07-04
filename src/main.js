802bc1b857b779b6d44032aec61d3f017fd71d33
Luthfi Dwi Pratama <138696359+MasBotang@users.noreply.github.com>
2025-07-05 01:15:10 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -82,7 +82,7 @@ export async function subscribeUserForPush() {
 
 window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/jejak-cerita/sw.js')
       .then(registration => {
         console.log('Service Worker terdaftar dengan scope:', registration.scope);
         
